128a4d2a5e99b7373baf6397f20ef1670471a655
parahathalbayev-droid <parahathalbayev@gmail.com>
2026-03-10 12:22:44 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 # ====== ТВОИ ДАННЫЕ (уже вставлены) ======
 TOKEN = '8583869049:AAFHSW-sA26F03O_8uHUktatSqA_5FjYGhA'  # Твой токен
 ADMIN_CHAT_ID =6425578995   # ← ЗАМЕНИ ЭТО ЧИСЛО на свой ID (от @userinfobot)
-WEB_APP_URL = "https://cafe-menu.onrender.com"  # Твой адрес ngrok
+WEB_APP_URL = "https://cafe-menu-gzgz.onrender.com"  # Твой адрес ngrok
 # ==========================================
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -84,3 +84,4 @@ def main():
 if __name__ == '__main__':
 
     main()
+
